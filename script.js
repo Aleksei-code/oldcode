04eea39cae1c5667399e2d6fcc362f43aab4be0f
Aleksey-hub-z <matyunin17@gmail.com>
2020-12-06 19:22:32 +0300

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict';
+// to string
+
+// 1)
+//console.log(typeof(String(4)));
+
+//let ans = +prompt("Hello?", '');
+//console.log(ans)
+
+//let answer = +prompt ("are you here?", "Yes");                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+
+//console.log(typeof(answer));
+//console.log(answer);
+
+let incr = 10,
+    decr = 10;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = false,
+isClose = false;
+
+console.log(isChecked && isClose);
+console.log(isChecked || isClose);
